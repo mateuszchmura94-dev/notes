aa22c5311024e32b23b427e32becb798d383de0e
Mateusz <mateuszchmura94@gmail.com>
2026-08-06 08:07:34 +0200

    Added sql queries to create needed tables

---
+++ queries.sql
@@ -0,0 +1,22 @@
+CREATE TABLE band (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50) UNIQUE NOT NULL,
+add_date DATE,
+note VARCHAR,
+score int
+);
+
+CREATE TABLE album (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+add_date DATE,
+score INT,
+band_id int NOT NULL REFERENCES band(id)
+);
+
+CREATE TABLE songs (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+score INT,
+album_id int NOT NULL REFERENCES album(id)
+);
