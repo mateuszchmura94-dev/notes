191d062d5655e67c57e5a4596f009313e91d8d0f
Mateusz <mateuszchmura94@gmail.com>
2026-08-12 17:44:29 +0200

    Adding new band feature, .env.example file

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const API = "https://www.theaudiodb.com/api/v1/json/123/search.php?s=";
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,19 +31,58 @@ db.connect()
 //===========================================================================================================================
 //ROUTES
 //===========================================================================================================================
-
+async function fetchImg(bandName) {
+    const response = await axios.get(API + bandName);
+    const link = response.data.artists[0].strArtistThumb;
+    return link;
+}
 //Strona główna
 app.get("/", async (req, res) => {
     try {
         const result = await db.query("SELECT * FROM bands");
         const bands = result.rows;
+        await Promise.all(bands.map(async (band) => {
+            const pic = await fetchImg(band.name);
+            band.img = pic;
+        }))
+       
+       
+      //  bands.forEach(async (band) => {
+      ////      const pic = await fetchImg(band.name);
+      //      band.img = pic;
+      //      console.log(pic);
+      //  });
+       
+        //obr.artists[0].strArtistThumb
         console.log(bands);
         res.render("index.ejs", { bandList: bands });
     } catch (err) {
         console.error("Błąd podczas pobierania listy zespołów:", err.stack);
         res.status(500).send("Wystąpił błąd serwera");
     }
+    
 
+});
+app.post("/delete:id", async (req, res) => {
+        console.log(req.params.id);
+        res.redirect("/");
+    
+});
+app.post("/edit:id", async (req, res) => {
+        console.log(req.params.id);
+        res.redirect("/");
+    
+});
+
+app.post("/add", async (req, res) => {
+    const { bandName, bandScore, bandNote } = req.body;
+    try {
+        await db.query("INSERT INTO bands (name, score, note) VALUES ($1, $2, $3)", [bandName, bandScore, bandNote]);
+        res.redirect("/");
+    } catch (err) {
+        console.error("Błąd podczas dodawania zespołu:", err.stack);
+        res.status(500).send("Wystąpił błąd serwera");
+    }
 });
 
 //Listening
